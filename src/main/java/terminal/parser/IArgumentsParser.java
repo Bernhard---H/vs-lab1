a94631930496f89bfcaa2a0094745b3cf25ec557
Bernhard Halbartschlager <halbart.bernhard+git@gmail.com>
2016-11-16 00:34:49 +0100

    .

--- src/main/java/terminal/parser/IArgumentsParser.java
+++ src/main/java/terminal/parser/IArgumentsParser.java
@@ -1,6 +1,6 @@
 package terminal.parser;
 
-import terminal.exceptions.impl.ArgumentParseException;
+import terminal.exceptions.ParseException;
 import terminal.model.IArguments;
 
 /**
@@ -9,6 +9,6 @@ import terminal.model.IArguments;
 public interface IArgumentsParser<T extends IArguments> {
 
 
-    T parse(String parameter) throws ArgumentParseException;
+    T parse(String parameter) throws ParseException;
 
 }
