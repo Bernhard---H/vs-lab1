4a0b4ff90336984088188006fac7ff0cc6384c0b
Bernhard Halbartschlager <halbart.bernhard+git@gmail.com>
2016-11-17 00:40:42 +0100

    reflektieren

--- readme.txt
+++ readme.txt
@@ -1,6 +1,21 @@
 Reflect about your solution!
 
-https://sketchboard.me/vz9WWghDBlMq#/
 
 Summary:
-- TODO
+- alles implementiert
+- !exit am server beendet auch alle clients
+- beenden und schließen von ressourcen wird nicht synchronisiert (zu komplex)
+- client und server erzeugen ~1000 threads
+- zum teil wilde hacks um halbwegs korrektes interrupt handling zu ermöglichen
+- sehr kurze timeouts (fast busy waiting) um schnell genung für automatische tests zu sein
+
+die  InputStream (TestInputStream) klasse muss die available() methode implementieren!
+z.B.:
+    @Override
+    public int available() throws IOException {
+        if (this.in != null){
+            return 1;
+        }
+        return this.lines.size();
+    }
+sonst ist kein ordentliches interrupt handling möglich, weil read blockiert und interrupts ignoriert
--- src/test/java/util/TestInputStream.java
+++ src/test/java/util/TestInputStream.java
@@ -16,82 +16,91 @@ import java.util.concurrent.TimeUnit;
  * verification purposes.
  */
 public class TestInputStream extends InputStream {
-	private volatile BlockingQueue<String> lines = new LinkedBlockingQueue<>();
-	private InputStream in;
+    private volatile BlockingQueue<String> lines = new LinkedBlockingQueue<>();
+    private InputStream in;
 
-	@Override
-	public synchronized int read() throws IOException {
-		if (in == null) {
-			if ((in = nextLine()) == null) {
-				return -1;
-			}
-		} else if (in.available() <= 0) {
-			try {
-				return -1;
-			} finally {
-				in = null;
-			}
-		}
-		return in != null ? in.read() : -1;
-	}
+    @Override
+    public synchronized int read() throws IOException {
+        if (in == null) {
+            if ((in = nextLine()) == null) {
+                return -1;
+            }
+        } else if (in.available() <= 0) {
+            try {
+                return -1;
+            } finally {
+                in = null;
+            }
+        }
+        return in != null ? in.read() : -1;
+    }
 
-	/**
-	 * Returns a copy of the lines available for reading.
-	 *
-	 * @return the available lines
-	 * @throws IOException if the stream is closed
-	 */
-	public List<String> getLines() throws IOException {
-		List<String> copy = new ArrayList<>();
-		do {
-			try {
-				copy.add(lines.take());
-			} catch (InterruptedException e) {
-				throw new IOException(e);
-			}
-		} while (lines.size() > 0);
-		return copy;
-	}
+    /**
+     * Returns a copy of the lines available for reading.
+     *
+     * @return the available lines
+     * @throws IOException if the stream is closed
+     */
+    public List<String> getLines() throws IOException {
+        List<String> copy = new ArrayList<>();
+        do {
+            try {
+                copy.add(lines.take());
+            } catch (InterruptedException e) {
+                throw new IOException(e);
+            }
+        } while (lines.size() > 0);
+        return copy;
+    }
 
-	/**
-	 * Adds the given line to the input queue.
-	 *
-	 * @param line the line to add
-	 */
-	public void addLine(String line) {
-		lines.add(line);
-	}
+    /**
+     * Adds the given line to the input queue.
+     *
+     * @param line the line to add
+     */
+    public void addLine(String line) {
+        lines.add(line);
+    }
 
-	/**
-	 * Prepares the run line available for reading from it.
-	 * <p/>
-	 * This method blocks until a line is available or the stream becomes closed.
-	 *
-	 * @return the {@link InputStream} holding the line
-	 * @throws IOException if the stream is closed
-	 */
-	private InputStream nextLine() throws IOException {
-		try {
-			String line = null;
-			while (lines != null && line == null) {
-				line = lines.poll(500L, TimeUnit.MILLISECONDS);
-			}
-			if (line != null) {
-				return new ByteArrayInputStream((line.endsWith("\n") ? line : line + '\n').getBytes());
-			} else {
-				return new ByteArrayInputStream("".getBytes());
-			}
-		} catch (InterruptedException e) {
-			throw new IOException(e);
-		}
-	}
 
-	@Override
-	public void close() throws IOException {
-		if (in != System.in) {
-			super.close();
-		}
-		lines = null;
-		Thread.currentThread().interrupt();
-	}
+    @Override
+    public int available() throws IOException {
+        if (this.in != null){
+            return 1;
+        }
+        return this.lines.size();
+    }
+
+    /**
+     * Prepares the run line available for reading from it.
+     * <p/>
+     * This method blocks until a line is available or the stream becomes closed.
+     *
+     * @return the {@link InputStream} holding the line
+     * @throws IOException if the stream is closed
+     */
+    private InputStream nextLine() throws IOException {
+        try {
+            String line = null;
+            while (lines != null && line == null) {
+                line = lines.poll(500L, TimeUnit.MILLISECONDS);
+            }
+            if (line != null) {
+                return new ByteArrayInputStream((line.endsWith("\n") ? line : line + '\n').getBytes());
+            } else {
+                return new ByteArrayInputStream("".getBytes());
+            }
+        } catch (InterruptedException e) {
+            throw new IOException(e);
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        if (in != System.in) {
+            super.close();
+        }
+        lines = null;
+        Thread.currentThread().interrupt();
+    }
 }
